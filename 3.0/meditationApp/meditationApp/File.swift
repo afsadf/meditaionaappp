867d2e76ff3c85ce69bb0cd600baec7dc134545c
afsadf <50741380+afsadf@users.noreply.github.com>
2021-04-15 18:07:05 +0300

    Add files via upload

---
+++ 3.0/meditationApp/meditationApp/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  meditationApp
+//
+//  Created by user on 15.04.2021.
+//
+
+import Foundation
---
+++ 3.0/meditationApp/meditationApp/MainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainViewController.swift
+//  meditationApp
+//
+//  Created by user on 15.04.2021.
+//
+
+import UIKit
+import Alamofire
+import swiftyJSON
+
+
+class MainViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    @IBOutlet weak var userPictures: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
